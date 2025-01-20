430da8574ac7c672c1913c0081e2caa38a5d97e5
A706\1dam <manucarrera34@gmail.com>
2025-01-20 20:46:41 +0100

    cosas nuevas

--- objetos/src/main/java/org/example/Main.java
+++ objetos/src/main/java/org/example/Main.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Persona persona1=new Persona("Pepe", "Sanchéz", "12345678A", 80, 30, "Madrid", "Persiana");
+        Persona persona2=new Persona();
+
+        persona1.imprimirInfoPersona();
+        persona2.imprimirInfoPersona();
+        System.out.println(persona1.concatenar());
+
+        System.out.println("Antes: " + persona1.getNombre());
+        persona1.setNombre("Pepa");
+        System.out.println("Despues: " + persona1.getNombre());
+        persona1.setNombre("messi");
+        System.out.println("Madrit: " + persona1.getNombre());
     }
 }
