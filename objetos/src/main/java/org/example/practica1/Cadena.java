f1367f7fa3a36eab741bb7715bfd9cf74889b3c5
A706\1dam <manucarrera34@gmail.com>
2025-02-03 21:15:07 +0100

    Practica 1

---
+++ objetos/src/main/java/org/example/practica1/Cadena.java
@@ -0,0 +1,34 @@
+package org.example.practica1;
+
+import java.util.ArrayList;
+
+public class Cadena {
+
+    private String nombre;
+    private ArrayList<Programa> listaPrograma;
+
+    public Cadena(String nombre){
+        this.nombre=nombre;
+        listaPrograma=new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ArrayList<Programa> getListaPrograma() {
+        return listaPrograma;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Cadena{" +
+                "nombre='" + nombre + '\'' +
+                ", listaPrograma=" + listaPrograma +
+                '}';
+    }
+}
---
+++ objetos/src/main/java/org/example/practica1/Empleado.java
@@ -0,0 +1,70 @@
+package org.example.practica1;
+
+import java.util.Arrays;
+
+public class Empleado {
+    private static int numId=0;
+    final static String IDEP = "EP";
+    final static String[] CARGOS = {"director","tecnico","presentador","colaborador"};
+
+    private String id;
+    private String nombre;
+    private String cargo;
+    private Empleado director;
+
+    public Empleado(String nombre, String cargo){
+        id=IDEP+asignarId();
+        this.nombre=nombre;
+        this.cargo=asignarCargo(cargo);
+        asignarDirector(cargo);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public Empleado getDirector() {
+        return director;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String asignarId(){
+        return String.format("%03d",numId);
+    }
+
+    public String asignarCargo(String cargo){
+        for (int i = 0; i < CARGOS.length; i++) {
+            if (CARGOS[i].equals(cargo)){
+                return cargo;
+            }
+        }
+        return "pte";
+    }
+
+    public void asignarDirector(String cargo){
+        if (cargo.equals("director")){
+            director=null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", cargo='" + cargo + '\'' +
+                ", director=" + director +
+                '}';
+    }
+}
