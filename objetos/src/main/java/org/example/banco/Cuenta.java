98aa67efacba71b786027b248a5f49849a7353d3
A706\1dam <manucarrera34@gmail.com>
2025-02-07 16:28:38 +0100

    Preparasao

---
+++ objetos/src/main/java/org/example/banco/Cuenta.java
@@ -0,0 +1,20 @@
+package org.example.banco;
+
+public class Cuenta {
+    private static int numIBAN=0;
+
+    private String iban;
+    private Cliente titular;
+    private double saldo;
+
+    public Cuenta(){
+        iban=asignarID();
+        titular=null;
+        saldo=0;
+    }
+
+    public String asignarID(){
+        numIBAN++;
+        return String.format("%X3d",numIBAN);
+    }
+}
